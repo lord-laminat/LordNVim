757e4c5637fbbeade89d9c1e44f2585b29ee63e7
root <root@TOXA-XIAOMI>
2024-05-31 22:51:53 +0300

    add mason.lua

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -40,6 +40,7 @@ require("lazy").setup({
   { "hrsh7th/cmp-buffer" },
   { "hrsh7th/cmp-path" },
   { "hrsh7th/cmp-cmdline" },
-  { "hrsh7th/nvim-cmp" }
+  { "hrsh7th/nvim-cmp" },
 
+  { "williamboman/mason.nvim", build=":MasonUpdate" }
 })
