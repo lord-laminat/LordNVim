15dd79c6c9ab1d831300b182ecb7ba7469054697
root <root@TOXA-XIAOMI>
2024-06-03 12:09:25 +0300

    fix plugins.lua

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,6 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.syste({
+  vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
